7545953af948c3cc1aae0a6e2dba151136d2612c
olivier lamy <olamy@apache.org>
2018-03-06 14:13:44 +1000

    simplify
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/org/olamy/App.java
+++ src/main/java/org/olamy/App.java
@@ -4,19 +4,15 @@ import java.util.Arrays;
 
 /**
  * Hello world!
- *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
 
-        for (String prop : Arrays.asList("java.version","java.runtime.version","java.vm.version"
-                       ,"java.specification.version"
-                       ,"java.vm.specification.version") )
-        {
-            System.out.println( prop + " :" + System.getProperty( prop ) );
-        }
+        Arrays.asList( "java.version", "java.runtime.version", "java.vm.version", "java.specification.version",
+                       "java.vm.specification.version" ).stream().forEach(
+            prop -> System.out.println( prop + " :" + System.getProperty( prop ) ) );
 
 
     }
