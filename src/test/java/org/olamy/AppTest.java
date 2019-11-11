a7677b409c56acb036538b9ab8000967aab952ae
olivier lamy <olamy@apache.org>
2019-11-11 14:25:08 +1000

    nfailing rerun test
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/test/java/org/olamy/AppTest.java
+++ src/test/java/org/olamy/AppTest.java
@@ -1,8 +1,8 @@
 package org.olamy;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
@@ -10,29 +10,15 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+        System.out.println("testing");
+        assertTrue( false );
     }
 }
